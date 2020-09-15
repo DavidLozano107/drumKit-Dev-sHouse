8bfad488e15909cf498f1ebf16af2d927d601261
David Lozano <Davidlozano107@gmail.com>
2020-09-14 21:04:14 -0500

    Agregué bootstrap y funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
-//Variables
-const platillo = document.querySelector(".platillo");
-const img = document.querySelector(".img1");
-console.log(img.src);
+//Variables -Selectores
+const hithatSelector = document.querySelector("#hithat-selector");
+const drumkit = document.querySelector("#drumkit");
+
+//Clases
+class UI {
+    changeImage(img) {
+        drumkit.src = `./../img/${img}`;
+    }
+
+    backToBaseImg() {
+        setTimeout(() => {
+            //Cambia la img
+            ui.changeImage("base.png");
+        }, 300);
+    }
+}
+
+//Instancia de User interface
+const ui = new UI();
+
+//Escucha Todos los eventos
+eventLisneteners();
 
 //Eventos
-platillo.addEventListener("click", eventoPlatillo1);
+function eventLisneteners() {
+    //Escucha el veneto click en el hit-hat
+    hithatSelector.addEventListener("click", () => play("hit-hat.png"));
+}
 
 //Funciones
-function eventoPlatillo1() {
-  console.log("Plsss");
-
-  img.src = "./../hit-hat.png";
-
-  setTimeout(() => {
-    img.src = "./../base-caja.png";
-  }, 200);
+function play(img /*Sonido*/) {
+    //Sonar bateria
+    /**
+     * Invocar la funcion para que suene
+     */
+    console.log("Plsss");
+    //Cambia la img
+    ui.changeImage(img);
+    //Regresa a la base
+    ui.backToBaseImg();
 }
+
+//TODO: Funcion que suene
