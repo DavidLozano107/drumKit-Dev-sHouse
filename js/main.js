4529b86787995984fa68d29495d294eb9a322f91
David Lozano <Davidlozano107@gmail.com>
2020-09-20 12:34:47 -0500

    Añadi la funcion para reproducir el sonido de la bateria

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ const hithatSelector = document.querySelector("#hithat-selector");
 const drumkit = document.querySelector("#drumkit");
 
 //Clases
-class UI {
-    changeImage(img) {
-        drumkit.src = `./../img/${img}`;
-    }
 
-    backToBaseImg() {
-        setTimeout(() => {
-            //Cambia la img
-            ui.changeImage("base.png");
-        }, 300);
-    }
+class UI {
+  changeImage(img) {
+    drumkit.src = `./../img/${img}`;
+  }
+
+  backToBaseImg() {
+    setTimeout(() => {
+      //Cambia la img
+      ui.changeImage("base.png");
+    }, 300);
+  }
 }
 
 //Instancia de User interface
@@ -24,21 +25,25 @@ eventLisneteners();
 
 //Eventos
 function eventLisneteners() {
-    //Escucha el veneto click en el hit-hat
-    hithatSelector.addEventListener("click", () => play("hit-hat.png"));
+  //Escucha el veneto click en el hit-hat
+  hithatSelector.addEventListener("click", () =>
+    play("hit-hat.png", "hit-hat.mp3")
+  );
 }
 
 //Funciones
-function play(img /*Sonido*/) {
-    //Sonar bateria
-    /**
-     * Invocar la funcion para que suene
-     */
-    console.log("Plsss");
-    //Cambia la img
-    ui.changeImage(img);
-    //Regresa a la base
-    ui.backToBaseImg();
+
+function play(img, soundDrumKit) {
+  //Sonar bateria
+  playSounds(soundDrumKit);
+  console.log("Plsss");
+  //Cambia la img
+  ui.changeImage(img);
+  //Regresa a la base
+  ui.backToBaseImg();
 }
 
-//TODO: Funcion que suene
+function playSounds(sound) {
+  let sounds = new Audio(`../sounds/${sound}`);
+  sounds.play();
+}
